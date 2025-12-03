696c8adb388a5581a47a227c1e9efffe2278427c
NCIBenMcEvoy2005 <x25331971@student.ncirl.ie>
2025-12-03 13:33:46 +0000

    Add files via upload
    Comments added to both pages
    Java still broken, will be fixed up tomorrow

--- finalRedoneFolderStructure/marineBiodiversity/java/marineJava.js
+++ finalRedoneFolderStructure/marineBiodiversity/java/marineJava.js
@@ -1,13 +1,19 @@
 // Quiz code for Marine Biodiversity Page by Ben McEvoy(25331971)
 
-alert("Java Active");
-let result = 0
-function check(answer){
-	if (answer == yay){
-		alert("Java Active");
-	}else if (answer == nay){
-		alert("Java inActive");
-	}else{
-		alert("ERROR!")
-	}
-}
+//alert("Java Active");
+//let answerOne = 1
+//let answerTwo = 2
+//let answerThree = 3
+//let answerFour = 4
+//let results = prompt("1 = yay, 2 = nay");
+
+//function quiz(results){
+//	if (result == 1){
+//		document.writeln("yay")
+//	}else if (result == 2){
+//		document.writeln("nay")
+//	}else{
+//		document.writeln("ERROR")
+//	}
+//}
+
