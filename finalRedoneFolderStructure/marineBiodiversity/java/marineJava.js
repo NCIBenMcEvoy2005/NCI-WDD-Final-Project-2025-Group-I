77e19dffe39d9d5776335b827902375b19b51a6b
NCIBenMcEvoy2005 <x25331971@student.ncirl.ie>
2025-12-03 12:33:59 +0000

    Add files via upload
    marine biodiversity
    - java partially added
    - few comments
    - new image
    
    Global
    - Background Image added

---
+++ finalRedoneFolderStructure/marineBiodiversity/java/marineJava.js
@@ -0,0 +1,13 @@
+// Quiz code for Marine Biodiversity Page by Ben McEvoy(25331971)
+
+alert("Java Active");
+let result = 0
+function check(answer){
+	if (answer == yay){
+		alert("Java Active");
+	}else if (answer == nay){
+		alert("Java inActive");
+	}else{
+		alert("ERROR! Dumbass cant follow simple instructions")
+	}
+}
